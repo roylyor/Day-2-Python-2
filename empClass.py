a95c0893c16bbfcf46098344910eb1f8114124ae
roylyor <roy.junhong@gmail.com>
2024-07-17 10:11:35 +0800

    Update empClass.py

--- empClass.py
+++ empClass.py
@@ -7,19 +7,5 @@ class Employee:
         self._last_name = last_name
         self._emp_number = emp_number
 
-    # getter
-    @property #decorator for getter
-    def first_name(self):
-        return self._first_name
-    @property
-    def last_name(self):
-        return self._last_name
-    @property
-    def emp_number(self):
-        return self._emp_number
-    @property
-    def full_name(self):
-        return self.first_name + " " + self.last_name
-
-    # remove setter
+    # branch1
   
